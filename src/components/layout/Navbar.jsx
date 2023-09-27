01c3114aefc203fa669974b5cc1f64765006e91c
Vlad-404 <vmijat21@gmail.com>
2023-09-27 15:28:57 +0200

    Add DaisyUI theme

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 function Navbar({title}) {
   return (
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
-      <div className="container px-6 bg-blue-300">
-        <div className="flex-none px-2 bg-green-500">
+      <div className="container px-6">
+        <div className="flex-none px-2">
           <Link to="/" className="text-lg font-bold align-middle">
             { title }
           </Link>
         </div>
-        <div className="flex-1 px-2 bg-orange-400">
+        <div className="flex-1 px-2">
           <div className="flex justify-end">
             <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
               Home
