c000466981b16db6aa789eb9f724a8b213ca08b7
Vlad-404 <vmijat21@gmail.com>
2023-09-27 15:32:46 +0200

    Add adittional styling to navbar component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 function Navbar({title}) {
   return (
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
-      <div className="container px-6">
-        <div className="flex-none px-2">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
           <Link to="/" className="text-lg font-bold align-middle">
             { title }
           </Link>
         </div>
-        <div className="flex-1 px-2">
+        <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
             <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
               Home
