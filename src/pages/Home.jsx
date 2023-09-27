bfd7443c87e0102615499ff27e6f4be94d0fce56
Vlad-404 <vmijat21@gmail.com>
2023-09-27 16:47:59 +0200

    Add some content and styling to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 function Home() {
     return (
       <>
-        <h1 className="text-3xl font-bold underline text-darkgreen">
-            Hello world!
-        </h1>
+        <div className="px-5 md:px-20">
+          <h1 className="text-3xl font-bold text-darkgreen text-center">
+              Welcome to How To Skylines
+          </h1>
+          <p class="text-justify mt-10">
+            This page was made to give you bits of information about some basic and advanced techniques and mechanics for the game Cities: Skylines. As Cities: Skylines 2 is soon to be released, some of the techniques can be applied to it too, but the whole website was designed in mind for the Skylines 1. The idea is to give you short tutorials for the topics I saw recurring mostly on the internet.
+          </p>
+        </div>
       </>
     )
   }
