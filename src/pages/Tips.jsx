baa40d90184c42d9fd178a441b8fd509c96db680
Vlad-404 <57156189+Vlad-404@users.noreply.github.com>
2023-10-20 14:44:47 +0200

    Add function to isolate the last param and use it as a component
    Created function (reusability) to extract the last parameter from an url and use it as  component for rendering

--- src/pages/Tips.jsx
+++ src/pages/Tips.jsx
@@ -2,14 +2,22 @@ import Sidenav from "../components/layout/Sidenav";
 import Tipwindow from "../components/layout/Tipwindow";
 
 let tip = Tipwindow;
-let urlArray = [];
 let url = window.location.href;
 
-let urlArray = url.split('/');
-let lastMember = urlArray.length - 1;
-let lastParam = urlArray[lastMember];
+function getLastParam(url) {
+    let urlArray = [];
+    
+    let urlArray = url.split('/');
+    let lastMember = urlArray.length - 1;
+    let lastParam = urlArray[lastMember];
 
-tip = lastParam.charAt(0).toUpperCase() + str.slice(1);
+    // capitalizes the first letter to comply with React component formating
+    tip = lastParam.charAt(0).toUpperCase() + lastParam.slice(1);
+
+    return tip;
+};
+
+getLastParam(url);
 
 function Tips(tip) {
     return(
