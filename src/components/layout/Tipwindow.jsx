c3aed0de156bb421d85c4758191f7b417f04f555
Vlad-404 <57156189+Vlad-404@users.noreply.github.com>
2023-10-20 13:35:10 +0200

    Add width and styling for Tipwindow.jsx

--- src/components/layout/Tipwindow.jsx
+++ src/components/layout/Tipwindow.jsx
@@ -1,6 +1,6 @@
 function Tipwindow() {
     return (
-        <div>
+        <div className="w-3/4 px-5">
             <h1 className="px-10 mb-10 text-3xl">Get started</h1>
             <p className="text-justify">
                 Choose what you are most interested in from the navigation menu on the left, and the content will display here.
