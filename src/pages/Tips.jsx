c9758d63917b9645ce5d4fc31e930c896f3275a5
Vlad-404 <57156189+Vlad-404@users.noreply.github.com>
2023-10-20 14:21:18 +0200

    Add conditional rendering based on the endpoint

--- src/pages/Tips.jsx
+++ src/pages/Tips.jsx
@@ -1,12 +1,24 @@
 import Sidenav from "../components/layout/Sidenav";
 import Tipwindow from "../components/layout/Tipwindow";
 
-function Tips() {
+let tip = Tipwindow;
+let urlArray = [];
+let url = window.location.href;
+
+let urlArray = url.split('/');
+let lastMember = urlArray.length - 1;
+let lastParam = urlArray[lastMember];
+
+tip = lastParam.charAt(0).toUpperCase() + str.slice(1);
+
+function Tips(tip) {
     return(
         <article className="w-screen min-h-96 pt-16">
             <div className="flex flex-row">
                 <Sidenav />
-                <Tipwindow />
+                {/* proposed routing */}
+                <{tip} />
+                {/*<Tipwindow /> */}
             </div>
 
         </article>
