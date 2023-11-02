3bde50f6b0f2a87b06a1eabbd2956c722848abdd
Vlad-404 <57156189+Vlad-404@users.noreply.github.com>
2023-11-02 09:42:46 +0100

    Add Article component to Cityentry.jsx

--- src/components/tips/Cityentry.jsx
+++ src/components/tips/Cityentry.jsx
@@ -1,31 +1,35 @@
+import Article from "../shared/Article"
+
 function Cityentry() {
     return(
-        <div className="w-3/4 xl:w-1/2 px-5">
-            <div className="px-5 prose max-w-none text-black dark:text-white">
-                <h2 className="px-10 mb-10 dark:text-white text-black">Connection to the highway</h2>
-                <p className="text-justify">
-                    So you want to start a new city. You might have thought about how to organize it, where to put what or just wanted to throw random stuff in it to test them. In any case, the very first obstacle you encounter is how do you connect to the highway?
-                </p>
-                <h3 className="mt-5 mb-2 dark:text-white text-black">A roundabout</h3>
-                <p className="text-justify">
-                    This is the most common solution and it helps you quickly start a new city and get the ideas out there. Build a simple 2 way road, and the one way road will unlock. Now you can <a href="/" className="dark:text-white dark:underline-white text-black">build your own roundabout</a>. Depending on your city development, it will have to be replaced eventually, so leave plenty of space for an upgrade.
-                </p>
-        
-                <h3 className="mt-5 mb-2 dark:text-white text-black">Avenue</h3>
-                <p className="text-justify">
-                    This is the simplest one. It evolves after your city has grown a bit. You can continue the highway exit with an avenue, but you will have to use 2 one way streets to connect the avenue to the highway exit. Minimum effort and you can make the avenue as long as you wish. This entrance will have to be replaced very soon, depending on your city demands and is prone to traffic issues. It will serve you a short time, but by then you will have enough money to upgrade it to something better.
-                </p>
-                
-                <h3 className="mt-5 mb-2 dark:text-white text-black">Interchange(M)</h3>
-                <p className="text-justify">
-                    This one is only available to modded version of Skylines, or the ones that have everything unlocked. You can also build this with vanilla Skylines, but the highway is often outside of the buildable area. This costs the most out of the three options and is not recommended if you play with a budget. You can build a custom interchange of your own design or you can import something from the workshop. It would be good if you already had a general idea how your city will look like so you can build it right now and later focus on your city development.
-                </p>
-
-                <p className="text-justify border-t border-slate-300 mt-5 pt-2">
-                    All of these entrance have good and bad sides. The most important thing to know is that you can always change this. For that reason, it is good to leave some extra space for the entrance, because it's not easy to demolish that level 5 house or business that's in the way of progress.
-                </p>
+        <Article>
+            <div className="w-3/4 xl:w-1/2 px-5">
+                <div className="px-5 prose max-w-none text-black dark:text-white">
+                    <h2 className="px-10 mb-10 dark:text-white text-black">Connection to the highway</h2>
+                    <p className="text-justify">
+                        So you want to start a new city. You might have thought about how to organize it, where to put what or just wanted to throw random stuff in it to test them. In any case, the very first obstacle you encounter is how do you connect to the highway?
+                    </p>
+                    <h3 className="mt-5 mb-2 dark:text-white text-black">A roundabout</h3>
+                    <p className="text-justify">
+                        This is the most common solution and it helps you quickly start a new city and get the ideas out there. Build a simple 2 way road, and the one way road will unlock. Now you can <a href="/" className="dark:text-white dark:underline-white text-black">build your own roundabout</a>. Depending on your city development, it will have to be replaced eventually, so leave plenty of space for an upgrade.
+                    </p>
+            
+                    <h3 className="mt-5 mb-2 dark:text-white text-black">Avenue</h3>
+                    <p className="text-justify">
+                        This is the simplest one. It evolves after your city has grown a bit. You can continue the highway exit with an avenue, but you will have to use 2 one way streets to connect the avenue to the highway exit. Minimum effort and you can make the avenue as long as you wish. This entrance will have to be replaced very soon, depending on your city demands and is prone to traffic issues. It will serve you a short time, but by then you will have enough money to upgrade it to something better.
+                    </p>
+                    
+                    <h3 className="mt-5 mb-2 dark:text-white text-black">Interchange(M)</h3>
+                    <p className="text-justify">
+                        This one is only available to modded version of Skylines, or the ones that have everything unlocked. You can also build this with vanilla Skylines, but the highway is often outside of the buildable area. This costs the most out of the three options and is not recommended if you play with a budget. You can build a custom interchange of your own design or you can import something from the workshop. It would be good if you already had a general idea how your city will look like so you can build it right now and later focus on your city development.
+                    </p>
+    
+                    <p className="text-justify border-t border-slate-300 mt-5 pt-2">
+                        All of these entrance have good and bad sides. The most important thing to know is that you can always change this. For that reason, it is good to leave some extra space for the entrance, because it's not easy to demolish that level 5 house or business that's in the way of progress.
+                    </p>
+                </div>
             </div>
-        </div> 
+        </Article>
     )
 };
 
